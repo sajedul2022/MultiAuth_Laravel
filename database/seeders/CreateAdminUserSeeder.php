37987b81b1c70021c5c70f5353c6e5ced314b92b
sajedul2022 <sajedul.idb.info@gmail.com>
2024-06-09 13:40:56 +0600

    Laravel 9 to Laravel 10

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -27,7 +27,7 @@ class CreateAdminUserSeeder extends Seeder
 
         $role = Role::create(['name' => 'Admin']);
 
-        $permissions = Permission::pluck('id','id')->all();
+        $permissions = Permission::pluck('id', 'id')->all();
 
         $role->syncPermissions($permissions);
 
